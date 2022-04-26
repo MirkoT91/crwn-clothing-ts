f6e90961d3fdb36bb9a6f3ef5b1e5d58778eeca0
Mirko <mirkotomasina@gmail.com>
2022-04-26 09:28:23 -0700

    Adding cart context and shopping icon as well as cart dropdown

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -44,9 +44,8 @@ const firebaseConfig = {
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef);
+
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot.exists());
 
     if(!userSnapshot.exists()) {
         const { displayName, email } = userAuth;
