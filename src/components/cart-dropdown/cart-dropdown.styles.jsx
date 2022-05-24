2ced0cad9beec3506f2439729e6925325ae1f10b
Mirko <mirkotomasina@gmail.com>
2022-05-24 11:40:43 -0700

    adding Stripe

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -8,8 +8,8 @@ import {
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
-  width: 300px;
-  height: 340px;
+  width: 400px;
+  height: 500px;
   display: flex;
   flex-direction: column;
   padding: 20px;
@@ -18,7 +18,6 @@ export const CartDropdownContainer = styled.div`
   top: 90px;
   right: 40px;
   z-index: 5;
-  overflow-y: scroll;
   ${BaseButton},
   ${GoogleSignInButton},
   ${InvertedButton} {
@@ -32,7 +31,8 @@ export const EmptyMessage = styled.span`
 `;
 
 export const CartItems = styled.div`
-  height: 240px;
+  height: 400px;
   display: flex;
   flex-direction: column;
-`;
+  overflow: scroll;
+`
--- src/store/user/user.reducers.js
+++ src/store/user/user.reducers.js
@@ -21,4 +21,4 @@ export const userReducer = (state = INITIAL_STATE, action) => {
     default:
       return state;
   }
-};
+}; 
