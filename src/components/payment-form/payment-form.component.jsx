9d3a9c6df669049baf323edc6b9d4654375cc77b
Mirko <mirkotomasina@gmail.com>
2022-05-31 14:37:06 -0700

    Removed unused code from payment-form

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -18,10 +18,12 @@ const PaymentForm = () => {
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (e) => {
+
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
+
     setIsProcessingPayment(true);
     const response = await fetch('/.netlify/functions/create-payment-intent', {
       method: 'post',
@@ -29,31 +31,28 @@ const PaymentForm = () => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ amount: amount * 100 }),
-    }).then((res) => {
-      return res.json();
-    });
-
-    const clientSecret = response.paymentIntent.client_secret;
-
-    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+    }).then((res) => res.json());
+    const {
+      paymentIntent : {client_secret }
+    } = response;
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: currentUser ? currentUser.displayName : 'Yihua Zhang',
-        },
-      },
+          name: 'Mirko Tomasina'
+        }
+      }
     });
-
-    setIsProcessingPayment(false);
-
-    if (paymentResult.error) {
-      alert(paymentResult.error.message);
+    if(paymentResult.error) {
+      alert(paymentResult.error)
     } else {
-      if (paymentResult.paymentIntent.status === 'succeeded') {
-        alert('Payment Successful!');
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
       }
     }
-  };
+  }
+   
+
 
   return (
     <PaymentFormContainer>
